ab2f41b33ba46045b18377a68b83b82c68225d88
Abhranil Das <abhranil.das@utexas.edu>
2019-05-27 13:39:19 -0500

    n-d functionality

--- accuracy_obs_optimize.m
+++ accuracy_obs_optimize.m
@@ -1,18 +1,8 @@
 function acc_obs=accuracy_obs_optimize(dim,x,obs_a,obs_b)
 
-if dim==1
-    a2=x(1);
-    a1=x(2);
-    a0=x(3);
-elseif dim==2
-    a2=[x(1) x(2); x(3) x(4)];
-    a1=[x(5); x(6)];
-    a0=x(7);
-elseif dim==3
-    a2=[x(1) x(2) x(3); x(4) x(5) x(6); x(7) x(8) x(9)];
-    a1=[x(10); x(11); x(12)];
-    a0=x(13);
-end
+a2=reshape(x(1:dim^2),[dim dim])';
+a1=x(dim^2+1:dim^2+dim);
+a0=x(end);
 
 acc_obs=-sum([dot(obs_a,obs_a*a2',2) + obs_a*a1 + a0 > 0;...
     dot(obs_b,obs_b*a2',2) + obs_b*a1 + a0 < 0]);
